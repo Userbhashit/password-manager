153abf31475207e0d0be0201301e011c19d88ba3
Bhashit Gautam <bhashitgtm004@gmail.com>
2025-04-12 09:57:38 +0530

    Save user created password.

---
+++ save.py
@@ -0,0 +1,36 @@
+import sqlite3
+import datetime
+from cryptography.fernet import Fernet
+
+def save():
+    website = input("Name of the website or label: ")
+
+    while not website:
+        print("Label cannot be empty.")
+        website = input("Name of the website or label: ")
+
+    username = input("username: ")
+
+    password = input("password: ")
+    while not password:
+        print("Password cannot be empty.")
+        password = input("password: ")
+
+    with open(".key", "rb") as key_file:
+        key = key_file.read()
+
+    key = Fernet(key)
+    password = key.encrypt(password.encode())
+        
+    date, time = str(datetime.datetime.now().replace(microsecond=0)).split(" ") 
+    created = f"{date} at {time}"
+
+    db = sqlite3.connect(".pass.db")
+    pointer = db.cursor()
+    pointer.execute("INSERT INTO passwords(label, username, password, date_created) VALUES (?, ?, ?, ?)", (website, username, password, created))
+    db.commit()
+    db.close()
+
+if __name__ == "__main__":
+    save()
+
